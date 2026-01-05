4bbb7d29c829f024ab60e8b9d4685c92c1da8091
lewmeta <dev.lewismeta@gmail.com>
2026-01-05 14:43:02 +0300

    update tailwind configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
