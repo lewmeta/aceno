6bd814219cb9fd1e0b73bb6ea14ba46670c7f08a
lewmeta <dev.lewismeta@gmail.com>
2026-01-05 14:36:54 +0300

    partial update

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,18 +13,21 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-zinc-900">
-            <div>
+        <div class="flex min-h-screen flex-col items-center bg-gray-100 sm:justify-center sm:pt-0 dark:bg-zinc-900">
+            {{-- <div>
                 <a href="/" wire:navigate>
                     <x-application-logo class="h-20 w-20 fill-current text-gray-500" />
                 </a>
-            </div>
+            </div> --}}
 
-            <div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-zinc-800">
+            <div class="h-screen w-full overflow-hidden bg-white shadow-md dark:bg-zinc-800">
                 {{ $slot }}
             </div>
         </div>
+
+        @livewireScripts
     </body>
 </html>
